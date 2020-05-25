bba145fcf946fd71040b0282e938a77c64176ca3
gabridego <41424282+gabridego@users.noreply.github.com>
2020-05-26 01:36:58 +0200

    Initial commit

---
+++ resnet_cifar.py
@@ -0,0 +1,261 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.backends import cudnn
+import math
+import time
+import torch.utils.model_zoo as model_zoo
+
+DEVICE = 'cuda'
+
+"""
+Credits to @hshustc
+Taken from https://github.com/hshustc/CVPR19_Incremental_Learning/tree/master/cifar100-class-incremental
+"""
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, parameters, num_classes=10):
+        self.inplanes = 16
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(block, 16, layers[0])
+        self.layer2 = self._make_layer(block, 32, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, layers[2], stride=2)
+        self.avgpool = nn.AvgPool2d(8, stride=1)
+        self.fc = nn.Linear(64 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+		
+        # hyperparameters
+        self.num_classes = parameters['NUM_CLASSES']
+        self.num_epochs = parameters['NUM_EPOCHS']
+        self.scheduler_parameters = parameters['SCHEDULER_PARAMETERS']
+        
+        self.criterion = parameters['CRITERION']()
+        self.optimizer = parameters['OPTIMIZER'](self.parameters(), **parameters['OPTIMIZER_PARAMETERS'])
+        self.scheduler = parameters['SCHEDULER']
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+		
+    def train(self, train_dataloader, val_dataloader, state_dict=None, verbose=False, validation_step=5):
+        self = self.to(DEVICE)
+        cudnn.benchmark
+
+        if state_dict:
+            self.load_state_dict(state_dict)
+        
+        epochs_stats = {}
+        last_time = time.time()
+        
+        optimizer = self.optimizer
+        scheduler = self.scheduler(optimizer, **self.scheduler_parameters)
+        
+        for epoch in range(self.num_epochs):
+            if verbose:
+                print('Epoch {:>3}/{}\tLoss: {:07.4f}\tLearning rate: {}'.format(
+                    epoch+1, self.num_epochs,
+                    loss.item() if len(epochs_stats) > 0 else -1,
+                    scheduler.get_last_lr()
+                ))
+              
+            for images, labels in train_dataloader:
+                #images, labels = (images.to(DEVICE), labels.to(DEVICE))
+                images, labels = (images.to(DEVICE), F.one_hot(labels, num_classes=self.num_classes).to(DEVICE, dtype=torch.float))
+
+                self.training = True
+
+                # PyTorch, by default, accumulates gradients after each backward pass
+                # We need to manually set the gradients to zero before starting a new iteration
+                optimizer.zero_grad()
+
+                # Forward pass to the network
+                outputs = self.forward(images)
+
+                # Compute loss based on output and ground truth
+                loss = self.criterion(outputs, labels)
+
+                # Compute gradients for each layer and update weights
+                loss.backward()  # backward pass: computes gradients
+                optimizer.step() # update weights based on accumulated gradients
+
+            epochs_stats[epoch] = {
+                'loss': loss.item(),
+                'learning_rate': scheduler.get_last_lr(),
+                'elapsed_time': time.time() - last_time
+            }
+            last_time = time.time()
+
+            # Evaluate accuracy on validation set if verbose at each validation step
+            if verbose and (epoch + 1) % validation_step == 0:
+                accuracy, _ = self.test(val_dataloader)
+                print(f'Epoch accuracy on validation set: {accuracy}')
+
+            # Step the scheduler
+            scheduler.step()
+        
+        return epochs_stats
+      
+    def test(self, dataloader):
+        self = self.to(DEVICE)
+        
+        with torch.no_grad():
+            self.training = False # Sets the module in evaluation mode
+
+            correct_predictions = 0
+            total_predictions = 0
+            prediction_history = {
+                'pred': [],
+                'true': [],
+            }
+
+            for images, labels in dataloader:
+                images, labels = (images.to(DEVICE), labels.to(DEVICE))
+
+                # Forward Pass
+                outputs = self.forward(images)
+
+                # Get predictions
+                _, preds = torch.max(outputs.data, 1)
+
+                # Update Corrects
+                correct_predictions += torch.sum(preds == labels.data).data.item()
+                total_predictions += len(labels)
+
+                prediction_history['pred'] += preds.tolist()
+                prediction_history['true'] += labels.data.tolist()
+
+        # Calculate Accuracy
+        accuracy = correct_predictions / float(total_predictions)
+
+        return accuracy, prediction_history
+
+def resnet20(parameters, pretrained=False, **kwargs):
+    n = 3
+    model = ResNet(BasicBlock, [n, n, n], parameters, **kwargs)
+    return model
+
+def resnet32(parameters, pretrained=False, **kwargs):
+    n = 5
+    model = ResNet(BasicBlock, [n, n, n], parameters, **kwargs)
+    return model
+
+def resnet56(parameters, pretrained=False, **kwargs):
+    n = 9
+    model = ResNet(Bottleneck, [n, n, n], parameters, **kwargs)
+    return model
