e158eb09d3f3e11ed9a0464c43ec3fb7eb15cfbf
Ivan D'Onofrio <d.onofrio.ivan@gmail.com>
2020-05-31 09:11:30 +0200

    Logging functions

--- resnet_cifar.py
+++ resnet_cifar.py
@@ -327,8 +327,13 @@ class ResNet(nn.Module):
             features = self.forward(self.exemplars[label]['exemplars'], True)
             self.exemplars[label]['mean'] = torch.mean(torch.stack(features), 0, keepdim=True)
 
+            print(self.exemplars[label]['mean'])
+
             # Store only m exemplars
             self.exemplars[label]['exemplars'] = random.sample(self.exemplars[label]['exemplars'], min(batch, count))
+
+            print(self.exemplars[label]['exemplars'])
+
             counter -= batch
 
 
